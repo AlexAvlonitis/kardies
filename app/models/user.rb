03faaf5b574846f947c948221dfaa21a77ea8f15
Alex <avlonitis@msn.com>
2016-08-11 22:39:43 +0100

    fixed error in user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,11 @@ class User < ApplicationRecord
   has_attached_file :profile_picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :profile_picture, content_type: /\Aimage\/.*\Z/
 
-  validates :username, presense: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 
-  validates_attachment :avatar,
+  validates_attachment :profile_picture,
     size:         { in: 0..10.megabytes },
     content_type: { content_type: /^image\/(jpeg|png|gif|tiff)$/ }
-  end
 
   def full_name
     self.first_name + " " + self.last_name if full_name_exists?
