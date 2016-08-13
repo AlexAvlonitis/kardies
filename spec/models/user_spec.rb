d79d35f4bd2fe2f71e724ad5c066d0741a772aee
Alex <avlonitis@msn.com>
2016-08-13 13:33:54 +0100

    added user unit testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe User do
   it { is_expected.to validate_uniqueness_of(:username) }
   it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
   it { is_expected.to validate_presence_of(:email) }
+
+  context 'methods' do
+    let(:user) { FactoryGirl.create(:user) }
+
+    context "full name complete exists" do
+      it "is expected to return the full name" do
+        expect(user.full_name).to eq 'Alex Avlonitis'
+      end
+    end
+
+    context "full name doesn't exist" do
+      it "is expected to return nothing" do
+        user.first_name = nil
+        expect(user.full_name).to eq nil
+      end
+    end
+  end
+
 end
