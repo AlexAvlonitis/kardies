835f13ac2360ee65ee772dbcb3b743929dd4fcae
Alex <avlonitis@msn.com>
2016-08-14 23:31:59 +0100

    added view country helpers to retrieve codes from dB

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def render_country(country)
+    CS.get[country.to_sym]
+  end
+
+  def render_state(country, state = nil)
+    "/ #{CS.get(country.to_sym)[state.to_sym]}" if state
+  end
+
+  def render_city(country, state = nil, city = nil)
+    "/ #{CS.get(country.to_sym, state.to_sym)[city.to_i]}" if state && city
+  end
 end
