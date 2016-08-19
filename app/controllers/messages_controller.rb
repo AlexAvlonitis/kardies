77ad42b11c8e43265958931b65a8c4c0328e6add
Alex <avlonitis@msn.com>
2016-08-19 23:16:17 +0100

    Added send message view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,30 @@
 class MessagesController < ApplicationController
+  before_action :set_user
+
+  def new
+    @message = @user.messages.build
+  end
+
+  def create
+    @message = @user.messages.build(message_params)
+
+    if @message.save
+      flash[:notice] = "Message has been sent."
+      redirect_to user_path(@user)
+    else
+      flash.now[:alert] = "Message has not been sent."
+      render 'new'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :body)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
     resources :posts
+    resources :messages
   end
 
   get 'countries/:country', to: 'places#states'
