86b459c590e383133ca3fe98ce6ad1d2d8f1c597
Alex <avlonitis@msn.com>
2016-10-06 20:39:31 +0100

    added pictures to aws

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'simple_form'
 gem 'acts_as_votable', '~> 0.10.0'
 gem 'greek-cities', '~> 0.0.1'
 gem 'chewy'
+gem 'aws-sdk'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -3,7 +3,10 @@ class UserDetail < ApplicationRecord
 
   update_index('users#user') { user }
 
-  has_attached_file :profile_picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :profile_picture, styles: {
+    medium: "300x300>",
+    thumb: "100x100>"
+  }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :profile_picture, content_type: /\Aimage\/.*\Z/
 
   validates_attachment :profile_picture,
