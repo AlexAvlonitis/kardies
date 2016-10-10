2ec5cbf03c3736d950661022f7e416d79f890950
Alex <avlonitis@msn.com>
2016-10-10 18:07:34 +0100

    added working message functionality, WIP: refactor

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,15 +1,61 @@
 class ConversationsController < ApplicationController
   before_action :get_mailbox
-  before_action :get_conversation, except: [:index]
+  before_action :get_conversation, except: [:index, :empty_trash]
+  before_action :get_box, only: [:index]
 
   def index
-    @conversations = @mailbox.inbox
+    if @box.eql? "inbox"
+      @conversations = @mailbox.inbox
+    elsif @box.eql? "sent"
+      @conversations = @mailbox.sentbox
+    else
+      @conversations = @mailbox.trash
+    end
   end
 
   def show
   end
 
+  def reply
+    current_user.reply_to_conversation(@conversation, params[:body])
+    flash[:success] = 'Reply sent'
+    redirect_to conversation_path(@conversation)
+  end
+
+  def destroy
+    @conversation.move_to_trash(current_user)
+    flash[:success] = 'The conversation was moved to trash.'
+    redirect_to conversations_path
+  end
+
+  def restore
+    @conversation.untrash(current_user)
+    flash[:success] = 'The conversation was restored.'
+    redirect_to conversations_path
+  end
+
+  def empty_trash
+    @mailbox.trash.each do |conversation|
+      conversation.receipts_for(current_user).update_all(deleted: true)
+    end
+    flash[:success] = 'Your trash was cleaned!'
+    redirect_to conversations_path
+  end
+
+  def mark_as_read
+    @conversation.mark_as_read(current_user)
+    flash[:success] = 'The conversation was marked as read.'
+    redirect_to conversations_path
+  end
+
   private
+
+  def get_box
+    if params[:box].blank? or !%w(inbox sent trash).include?(params[:box])
+      params[:box] = 'inbox'
+    end
+    @box = params[:box]
+  end
 
   def get_mailbox
     @mailbox ||= current_user.mailbox
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,17 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :conversations, only: [:index, :show, :destroy]
+  resources :conversations, only: [:index, :show, :destroy] do
+    member do
+      post :reply
+      post :restore
+      post :mark_as_read
+    end
+    collection do
+      delete :empty_trash
+    end
+  end
+
   get 'messages/:username/new', to: 'messages#new', as: :new_message
   post 'messages', to: 'messages#create', as: :messages
 
