ad097eea6342891c0ea311c980a5b695f2b44046
Alex Avlonitis <avlonitis@msn.com>
2016-10-18 22:35:13 +0100

    testing production server for actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.allowed_request_origins = [ 'http://vps193319.ovh.net', 'http://127.0.0.1', %r{http://local.*} ]
+  config.action_cable.url = 'ws://vps193319.ovh.net/cable'
+  config.action_cable.allowed_request_origins = [ 'http://vps193319.ovh.net', 'http://localhost', 'http://127.0.0.1' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
