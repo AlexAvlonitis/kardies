f393579d328cf155bd26888c78f2fdea286f5264
Alex Avlonitis <avlonitis@msn.com>
2016-11-12 02:01:45 +0000

    not allowed blank email from fb

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -9,13 +9,23 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def handle_redirect(_session_variable, kind)
     # Use the session locale set earlier; use the default if it isn't available.
     I18n.locale = session[:omniauth_login_locale] || I18n.default_locale
-    sign_in_and_redirect user, event: :authentication
-    set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
+    if ! omniauth_values.info.email.blank?
+      sign_in_and_redirect user, event: :authentication
+      set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
+    else
+      redirect_to root_path
+    end
   end
 
   def user
-    user = User.find_for_oauth(env['omniauth.auth'])
+    user = User.find_for_oauth(omniauth_values)
     UserDetail.find_or_create_by(user_id: user.id)
     user
+  end
+
+  private
+
+  def omniauth_values
+    env['omniauth.auth']
   end
 end
