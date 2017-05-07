3e7ffbab1853d55acc0bbc3558ac15cd8a751978
Alex <avlonitis@msn.com>
2017-05-07 13:24:02 +0100

    if conversation exists dont create a new one

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,10 +12,16 @@ class MessagesController < ApplicationController
   def create
     recipient = User.find_by_username(params[:message][:username])
     redirect_to users_path if recipient == current_user
-    current_user.send_message(recipient, params[:message][:body], current_user.username).conversation
-    add_conversation_notification(recipient)
-    flash[:success] = "Message has been sent!"
-    redirect_to users_path
+    existing_conversation = Mailboxer::Conversation.between(current_user, recipient).find { |c| c.participants.count == 2 }
+    if existing_conversation && !existing_conversation.is_trashed?(current_user)
+      current_user.reply_to_conversation(existing_conversation, params[:message][:body])
+      redirect_to conversation_path(existing_conversation)
+    else
+      receipt = current_user.send_message(recipient, params[:message][:body], current_user.username).conversation
+      flash[:success] = "Message has been sent!"
+      redirect_to conversation_path(receipt)
+      add_conversation_notification(recipient)
+    end
   end
 
   private
