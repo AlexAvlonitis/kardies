6b88b7af02a66e1ecb7ee560e0a84b35c1cb5409
Alex <avlonitis@msn.com>
2017-06-03 21:39:05 +0100

    deleted old foundation css files and added back the collapse burger

--- vendor/assets/javascripts/creative.js
+++ vendor/assets/javascripts/creative.js
@@ -5,12 +5,12 @@
     $(document).on('scroll', function() {
       if ($(window).width() > 768) {
         $('.navbar').css('background-color', '#3b5998')
-        $('.white').css('color', '#FFFFFF')
         $('.navbar-brand').css('color', '#FFFFFF')
+        $('.navbar a').css('color', '#FFFFFF')
         $('.navbar-brand').css('font-size', '20px')
         if ($(this).scrollTop() > 50) {
           $('.navbar').css('background-color', '#f5f5f5')
-          $('.white').css('color', '#F05F40')
+          $('.navbar a').css('color', '#F05F40')
           $('.navbar-brand').css('color', '#F05F40')
           $('.navbar-brand').css('font-size', '16px')
         }
