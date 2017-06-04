f2da9e06e4070fc1bad8571761c20e721f49e91c
Alex <avlonitis@msn.com>
2017-06-04 17:40:43 +0100

    better email notification logic and email sent on new conversation twice

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,26 +10,39 @@ class MessagesController < ApplicationController
   end
 
   def create
-    recipient = User.find_by_username(params[:message][:username])
-    redirect_to users_path if recipient == current_user
-    existing_conversation = Mailboxer::Conversation.between(current_user, recipient).find { |c| c.participants.count == 2 }
+    @recipient ||= User.find_by_username(params[:message][:username])
+    redirect_to users_path if @recipient == current_user
+    existing_conversation = find_existing_conversation
     if existing_conversation && !existing_conversation.is_trashed?(current_user)
       current_user.reply_to_conversation(existing_conversation, params[:message][:body])
+      add_conversation_notification
+      conversation_notification_email
       redirect_to conversation_path(existing_conversation)
     else
-      receipt = current_user.send_message(recipient, params[:message][:body], current_user.username).conversation
+      receipt = current_user.send_message(@recipient, params[:message][:body], current_user.username).conversation
       flash[:success] = "Message has been sent!"
       redirect_to conversation_path(receipt)
-      add_conversation_notification(recipient)
+      add_conversation_notification
+      conversation_notification_email
     end
   end
 
   private
+  attr_reader :conversation_notification_email
 
-  def add_conversation_notification(recipient)
-    ConversationNotification.create(user_id: recipient.id,
+  def add_conversation_notification
+    ConversationNotification.create(user_id: @recipient.id,
                                     receiver_id: current_user.id,
                                     received: true )
-    ConversationsNotificationEmail.new(recipient).send_email
+  end
+
+  def conversation_notification_email
+    @conversation_notification_email ||=
+      ConversationsNotificationEmail.new(@recipient).send_email
+  end
+
+  def find_existing_conversation
+    Mailboxer::Conversation.between(current_user, @recipient)
+                           .find { |c| c.participants.count == 2 }
   end
 end
--- app/lib/conversations_notification_email.rb
+++ app/lib/conversations_notification_email.rb
@@ -5,25 +5,27 @@ class ConversationsNotificationEmail
   end
 
   def send_email
-    if messages_email_allowed? && user_not_online
-      ConversationMailer.message_notification(user).deliver_later
+    if user.is_signed_in || email_has_been_sent
+      return nil
+    elsif email_allowed?
+      return ConversationMailer.message_notification(user).deliver_later
     else
-      nil
+      return nil
     end
   end
 
   private
   attr_reader :user
 
-  def messages_email_allowed?
-    user.email_preference.present? ? !!(user.email_preference.messages) : true
-  end
-
-  def user_not_online
-    if user.is_signed_in
-      false
-    elsif user.is_signed_in == false
+  def email_allowed?
+    if user.email_preference.present?
+      !!(user.email_preference.messages)
+    else
       true
     end
+  end
+
+  def email_has_been_sent
+    !!(user.conversation_notifications.present?)
   end
 end
