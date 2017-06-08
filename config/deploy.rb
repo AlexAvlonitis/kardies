73b2ce82bb5afc165b0f0a78f3d41c4df10e1986
Alex <avlonitis@msn.com>
2017-06-08 23:03:54 +0100

    revert ssl changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :deploy_via, :remote_cache
 set :pty, true
 
-set :nginx_use_ssl, true
+# set :nginx_use_ssl, true
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
