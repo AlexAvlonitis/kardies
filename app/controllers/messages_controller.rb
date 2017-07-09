29a77b8b92d8a1f3c80ea855f539fd3895a8d8b9
Alex <avlonitis@msn.com>
2017-07-09 18:53:49 +0100

    Refactored messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,4 @@
 class MessagesController < ApplicationController
-
   def new
     @recipient = User.find_by_username(params[:username])
     redirect_to users_path if @recipient == current_user
@@ -12,23 +11,24 @@ class MessagesController < ApplicationController
   def create
     @recipient ||= User.find_by_username(params[:message][:username])
     redirect_to users_path if @recipient == current_user
-    existing_conversation = find_existing_conversation
-    if existing_conversation && !existing_conversation.is_trashed?(current_user)
-      current_user.reply_to_conversation(existing_conversation, params[:message][:body])
-      add_conversation_notification
-      conversation_notification_email
-      redirect_to conversation_path(existing_conversation)
-    else
-      receipt = current_user.send_message(@recipient, params[:message][:body], current_user.username).conversation
-      flash[:success] = "Message has been sent!"
-      redirect_to conversation_path(receipt)
-      add_conversation_notification
-      conversation_notification_email
-    end
+    send_message
   end
 
   private
   attr_reader :conversation_notification_email
+
+  def send_message
+    conversation = find_existing_conversation
+    if conversation && !conversation_deleted?(conversation)
+      current_user.reply_to_conversation(conversation, params[:message][:body])
+    else
+      conversation = current_user.send_message(@recipient, params[:message][:body], current_user.username).conversation
+      flash[:success] = t ".sent"
+    end
+    redirect_to conversation_path(conversation)
+    add_conversation_notification
+    conversation_notification_email
+  end
 
   def add_conversation_notification
     ConversationNotification.create(user_id: @recipient.id,
@@ -44,5 +44,10 @@ class MessagesController < ApplicationController
   def find_existing_conversation
     Mailboxer::Conversation.between(current_user, @recipient)
                            .find { |c| c.participants.count == 2 }
+  end
+
+  def conversation_deleted?(conversation)
+    conversation.is_trashed?(current_user) ||
+      conversation.is_deleted(current_user)
   end
 end
