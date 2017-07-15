04dd739c087a15afeef7576759abd3a75f1cc897
Alex Avlonitis <avlonitis@msn.com>
2017-07-15 11:37:03 +0100

    chewy back to what it was

--- app/chewy/users_index.rb
+++ app/chewy/users_index.rb
@@ -7,13 +7,13 @@ class UsersIndex < Chewy::Index
     }
   }
 
-  define_type User do
+  define_type User.includes(:user_detail) do
     field :username, :email, :deleted_at
     field :is_signed_in, type: :boolean
-    field :state, value: ->(user) { user.state }
-    field :city, value: ->(user) { user.city }
-    field :age, value: ->(user) { user.age }
-    field :gender, value: ->(user) { user.gender }
+    field :state, value: ->(user) { user.user_detail.state }
+    field :city, value: ->(user) { user.user_detail.city }
+    field :age, value: ->(user) { user.user_detail.age }
+    field :gender, value: ->(user) { user.user_detail.gender }
     field :created, type: 'date', include_in_all: false,
       value: ->{ created_at } # value proc for source object context
   end
