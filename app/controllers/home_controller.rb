75e8f2f77f200a2cfc2f06af1e714ef241c31406
Alex <avlonitis@msn.com>
2017-09-01 22:37:56 +0100

    completed the basic design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    redirect_to user_path(current_user) if user_signed_in?
+    redirect_to users_path if user_signed_in?
     @user = User.new
   end
 end
