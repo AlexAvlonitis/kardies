13d1c782c78b72e2083a2a732680a3ed68ef86a3
Alex <avlonitis@msn.com>
2017-11-16 10:31:56 +0000

    extracted gender tupes into helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def gender_types
+    [
+      ["#{t '.gender_man'}", "male"],
+      ["#{t '.gender_woman'}", "female"],
+      ["#{t '.gender_other'}", "other"]
+    ]
+  end
 end
