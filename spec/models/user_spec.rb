88948f31357fc4d003bb2a853118ddec994ef88b
Alex <avlonitis@msn.com>
2017-12-09 11:40:10 +0000

    added callback test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,15 +12,23 @@ RSpec.describe User do
   it { should have_one(:user_detail) }
   it { should have_one(:about) }
 
-   describe 'uniqueness' do
-     subject { FactoryGirl.build(:user) }
+ describe 'persistense in db' do
+   subject { FactoryGirl.build(:user) }
 
-     before do
-       allow(subject).to receive(:auto_like) { true }
-     end
+   before do
+     allow(subject).to receive(:auto_like) { true }
+   end
 
+   context 'uniqueness' do
      it { is_expected.to validate_uniqueness_of(:username) }
      it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
    end
 
+   context 'callbacks' do
+     it 'sends an auto like after creation' do
+       expect(subject).to receive(:auto_like)
+       subject.save
+     end
+   end
+ end
 end
