a19eeef724fa4762666d50bce1d9eb61847fff4c
Alex <avlonitis@msn.com>
2017-12-09 11:29:57 +0000

    fix user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  subject { FactoryGirl.build(:user) }
+  subject { FactoryGirl.build_stubbed(:user) }
+
   it { is_expected.to have_db_index(:email) }
   it { is_expected.to have_db_index(:username) }
   it { is_expected.to validate_presence_of(:username) }
-  it { is_expected.to validate_uniqueness_of(:username) }
-  it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
   it { is_expected.to validate_presence_of(:email) }
   it { should have_many(:messages) }
-  it { should have_many(:galleries) }
+  it { should have_one(:gallery) }
   it { should have_one(:user_detail) }
   it { should have_one(:about) }
+
+   describe 'uniqueness' do
+     subject { FactoryGirl.build(:user) }
+
+     before do
+       allow(subject).to receive(:auto_like) { true }
+     end
+
+     it { is_expected.to validate_uniqueness_of(:username) }
+     it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+   end
 
 end
