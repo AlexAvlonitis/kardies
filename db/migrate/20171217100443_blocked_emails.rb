70596c7cdb6c6f5495c51ebe12c58e0ae9d195a0
Alex <avlonitis@msn.com>
2017-12-17 17:18:12 +0000

    remove change migration name

--- db/migrate/20171217100443_blocked_emails.rb
+++ db/migrate/20171217100443_blocked_emails.rb
@@ -1,6 +1,6 @@
 class BlockedEmails < ActiveRecord::Migration[5.0]
   def change
-    create_table :blocked_emails do |t|
+    create_table :blocked_email do |t|
       t.string :email
       t.timestamps
     end
