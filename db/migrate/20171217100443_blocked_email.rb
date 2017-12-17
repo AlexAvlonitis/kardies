cc8e1a9db45fe43636f2c8eacf01c42819206f32
Alex <avlonitis@msn.com>
2017-12-17 17:42:08 +0000

    rename change migration file

--- db/migrate/20171217100443_blocked_email.rb
+++
@@ -1,8 +0,0 @@
-class BlockedEmails < ActiveRecord::Migration[5.0]
-  def change
-    create_table :blocked_email do |t|
-      t.string :email
-      t.timestamps
-    end
-  end
-end
---
+++ db/migrate/20171217100443_create_blocked_emails.rb
@@ -0,0 +1,8 @@
+class CreateBlockedEmails < ActiveRecord::Migration[5.0]
+  def change
+    create_table :blocked_emails do |t|
+      t.string :email
+      t.timestamps
+    end
+  end
+end
