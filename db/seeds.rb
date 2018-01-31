637ba9053ad28d373402006a6ab9bb7299929b4b
Alex <avlonitis@msn.com>
2018-01-31 23:04:29 +0000

    fix personalities greek grammar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Personality.destroy_all
+
 PERSONALITIES.each do |personality|
   Personality.create(
     code: personality.first,
