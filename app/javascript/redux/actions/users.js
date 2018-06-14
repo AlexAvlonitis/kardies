5e757b6bbc41972fa08422581bad9fee8dc5151c
Alex <avlonitis@msn.com>
2018-06-14 21:03:35 +0100

    add loader on users fetch

--- app/javascript/redux/actions/users.js
+++ app/javascript/redux/actions/users.js
@@ -1,8 +1,10 @@
 import fetch from 'cross-fetch';
 import { fetchedResults } from './search'
+import { fetchingResults } from './search'
 
 export const getUsers = () => {
   return (dispatch) => {
+    dispatch(fetchingResults())
     fetch('/api/users')
       .then(res => res.json())
       .then(users => {
