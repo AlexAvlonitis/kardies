164d37d76001d27fc1f4cd1f8913343cf511cd7f
Alex <avlonitis@msn.com>
2018-07-02 23:00:01 +0100

    remove view middleware

--- config/application.rb
+++ config/application.rb
@@ -7,16 +7,13 @@ require 'active_job/railtie'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
-require 'action_view/railtie'
 require 'action_cable/engine'
-# require "sprockets/railtie"
-require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Travelhub
+module Kardies
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -25,8 +22,6 @@ module Travelhub
     config.i18n.default_locale = :el
     config.autoload_paths += %W(#{config.root}/lib)
 
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'localhost:3000', '127.0.0.1:3000'
@@ -37,6 +32,8 @@ module Travelhub
         )
       end
     end
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
     config.middleware.use Rack::Attack
   end
 end
