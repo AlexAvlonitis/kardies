30ec6a392a85cb1695788af836581ff72e940afc
Alex <avlonitis@msn.com>
2018-07-05 20:27:23 +0100

    permit places without auth

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class PlacesController < ApiController
+      skip_before_action :doorkeeper_authorize!
+
       def states
         @states ||= ::GC.states
         render json: @states, status: :ok
