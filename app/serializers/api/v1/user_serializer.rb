278205e202148cb0492c56738a37ab48e2d48512
Alex <avlonitis@msn.com>
2018-08-17 20:42:58 +0100

    add like date in words

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,6 +1,10 @@
+require 'action_view'
+
 module Api
   module V1
     class UserSerializer < ActiveModel::Serializer
+      include ActionView::Helpers::DateHelper
+
       has_one :user_detail
       has_one :about
 
@@ -8,10 +12,20 @@ module Api
                  :profile_picture,
                  :profile_picture_medium,
                  :like,
+                 :like_date,
                  :email
 
       def like
         current_user.voted_for? object if current_user
+      end
+
+      def like_date
+        like_time = object.votes.find_by(votable_id: current_user.id)
+        distance_of_time_in_words(like_time.updated_at, to_time) if like_time
+      end
+
+      def to_time
+        Time.now
       end
 
       def email
