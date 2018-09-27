a9cb42e29f6517d56b43406f697808902e5d8885
Alex <avlonitis@msn.com>
2018-09-27 21:05:35 +0100

    add password update controller method

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class PasswordsController < ::Devise::RegistrationsController
       skip_before_action :authenticate_scope!
-      skip_before_action :doorkeeper_authorize!, only: :create
+      skip_before_action :doorkeeper_authorize!, only: [:create, :update]
 
       def create
         self.resource = resource_class.send_reset_password_instructions(resource_params)
@@ -13,10 +13,27 @@ module Api
         render json: { message: I18n.t('users.show.password_sent') }, status: :ok
       end
 
+      def update
+        self.resource = resource_class.reset_password_by_token(resource_params)
+
+        if resource.errors.empty?
+          render json: { message: I18n.t('users.show.password_changed')}, status: :ok
+        else
+          set_minimum_password_length
+          render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
       private
 
       def resource_params
-        params.permit(:email, :password, :format)
+        params.permit(
+          :email,
+          :password,
+          :password_confirmation,
+          :format,
+          :reset_password_token
+        )
       end
     end
   end
