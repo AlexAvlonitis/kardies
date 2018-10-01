2f6237bb38d1c068aad6348a8430927c79111109
Alex <avlonitis@msn.com>
2018-10-01 14:30:37 +0100

    fix blocked user serializer

--- app/serializers/api/v1/blocked_user_serializer.rb
+++ app/serializers/api/v1/blocked_user_serializer.rb
@@ -4,7 +4,8 @@ module Api
       attributes :user
 
       def user
-        blocked_user = User.find(object.blocked_user_id)
+        blocked_user = User.find_by(id: object.blocked_user_id)
+        return [] unless blocked_user
         UserSerializer.new(blocked_user, {scope: current_user})
       end
     end
