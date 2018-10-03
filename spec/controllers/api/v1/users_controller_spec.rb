ec4d6cc49a4319e771a0547cd0b8c91e374fb951
Alex <avlonitis@msn.com>
2018-10-03 11:54:18 +0100

    fix user controller specs

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 RSpec.describe Api::V1::UsersController, type: :controller do
   login_user
 
-  let(:user2) do
-    FactoryBot.build_stubbed(:user, username: 'user2', email: 'user2@test.com')
+  let!(:user2) do
+    FactoryBot.create(:user, username: 'user2', email: 'user2@test.com')
   end
 
   describe 'GET #index' do
     it "renders the index template" do
-      get :index
+      get :index, format: :json
       assert_response :success
     end
   end
 
   describe 'GET #show' do
     it "renders the #show view" do
-      get :show, params: { username: 'asd' }
+      get :show, format: :json, params: { username: 'asd' }
       assert_response :success
     end
 
@@ -26,13 +26,15 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
 
       it "can't view other profiles" do
-        req = get :show, params: { username: user2.username }
-        expect(req).to redirect_to(users_path)
+        response = get :show, format: :json, params: { username: user2.username }
+
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body['errors']).to eq('You need a profile pic')
       end
 
       it "can view his own profile" do
-        req = get :show, params: { username: @user.username }
-        expect(req).not_to redirect_to(users_path)
+        req = get :show, format: :json, params: { username: @user.username }
+        expect(req).not_to redirect_to(api_users_path)
       end
     end
 
@@ -42,13 +44,17 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
 
       it "can't view the profile" do
-        req = get :show, params: { username: user2.username }
-        expect(req).to redirect_to(users_path)
+        response = get :show, format: :json, params: { username: user2.username }
+
+        error = "Ενέργεια απαγορεύθηκε, υπάρχει αποκλεισμός απο την μεριά σας" \
+                " ή τη μεριά του χρήστη"
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body['errors']).to eq(error)
       end
 
       it "can view his own profile" do
-        req = get :show, params: { username: @user.username }
-        expect(req).not_to redirect_to(users_path)
+        req = get :show, format: :json, params: { username: @user.username }
+        expect(req).not_to redirect_to(api_users_path)
       end
     end
   end
