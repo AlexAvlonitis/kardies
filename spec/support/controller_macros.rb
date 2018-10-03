7033d5ef717618783ce1ae9fdb2fe9cb6446299e
Alex <avlonitis@msn.com>
2018-10-03 11:43:46 +0100

    fix abouts controller

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,12 +1,13 @@
 module ControllerMacros
   def login_user
+    let(:token) { double :acceptable? => true, resource_owner_id: @user.id }
+
     before do
       allow_any_instance_of(User).to receive(:auto_like) { true }
       allow_any_instance_of(User).to receive(:send_welcome_mail) { true }
 
       @user ||= FactoryBot.create(:user)
-
-      sign_in @user
+      allow(controller).to receive(:doorkeeper_token) { token }
     end
   end
 end
