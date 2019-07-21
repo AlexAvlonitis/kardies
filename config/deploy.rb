1b15c56f1f784f36b373b28016d891feff9b1a42
Alex <avlonitis@msn.com>
2019-07-21 12:53:31 +0100

    add deploy.rb rbenv_map_bins setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.6.3'
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
 set :rbenv_roles, :all # default value
 
 set :nginx_use_ssl, true
