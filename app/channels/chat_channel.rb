ff59f00f9402b810a9251343ca9e361c40067035
Alex <avlonitis@msn.com>
2019-10-05 15:43:19 +0100

    Update vulnerable gem

---
+++ app/channels/chat_channel.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+# Action Cable runs in a loop that does not support auto reloading.
+
+class ChatChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "chat"
+  end
+end
