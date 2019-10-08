1c15e0c4c4fd0f52ca4f2ea6de48831dbb32bfba
jnascimento <jnascimento@dxc.com>
2019-10-08 17:40:17 -0300

    Creating tests for report class. It was simpler than I was thinking.

--- spec/controllers/api/v1/reports_controller_spec.rb
+++ spec/controllers/api/v1/reports_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ReportsController, type: :controller do
+    login_user
 
+  let!(:user) do
+    FactoryBot.create(:user, username: 'user', email: 'user@test.com')
+  end
+
+  describe 'POST #create' do
+    it "create new report" do
+      post :create, format: :json, params: { username: user.username }
+      assert_response :success
+    end
+  end
 end
