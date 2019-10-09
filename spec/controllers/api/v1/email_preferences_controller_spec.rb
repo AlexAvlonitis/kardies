d6d46cf17274bf0fa54097cb7ad33300bb4b64fa
Sathia <sathia.s@getsimpl.com>
2019-10-09 16:47:00 +0530

    fix(#33): using factory, rspec refactoring test description

--- spec/controllers/api/v1/email_preferences_controller_spec.rb
+++ spec/controllers/api/v1/email_preferences_controller_spec.rb
@@ -5,45 +5,43 @@ RSpec.describe Api::V1::EmailPreferencesController, type: :controller do
 
   describe "PUT #update" do
     context "when user has no email preference" do
-      it "should return success" do
+      it "returns success" do
         put :update, format: :json, params: {email_preference: {news: true}}
 
         assert_response :success
       end
 
-      it "should create and return new email preference" do
-        email_preference = {"news" => true, "likes" => false, "messages" => false}
+      it "creates and return new email preference" do
+        email_preference = FactoryBot.attributes_for(:email_preference, news: true, likes: false, messages: false)
         put :update, format: :json, params: {email_preference: email_preference}
+        parsed_body = JSON.parse(response.body, symbolize_names: true)
 
-        parsed_body = JSON.parse(response.body)
-        expect(parsed_body['email_preference']).to match(email_preference)
+        expect(parsed_body[:email_preference]).to match(email_preference)
       end
 
-      it "should create new email preference with true when invalid params are passed" do
+      it "creates new email preference with true when invalid params are passed" do
         put :update, format: :json, params: {email_preference: {invalid_params: true}}
 
-        parsed_body = JSON.parse(response.body)
-        expect(parsed_body['email_preference']).to match({"news" => true, "likes" => true, "messages" => true})
+        parsed_body = JSON.parse(response.body, symbolize_names: true)
+
+        email_preference = FactoryBot.attributes_for(:email_preference, news: true, likes: true, messages: true)
+        expect(parsed_body[:email_preference]).to match(email_preference)
       end
     end
 
     context "when user has aleady email preference" do
-      it "should update and return updated email preference" do
-        email_preference = @user.build_email_preference
-        email_preference.likes = true
-        email_preference.messages = true
-        email_preference.news = false
-        email_preference.save
-        email_preference = {"news" => false, "likes" => true, "messages" => true}
+      it "updates and return updated email preference" do
+        FactoryBot.create :email_preference, user_id: @user.id, likes: true, messages: true, news: true
+        email_preference = FactoryBot.attributes_for(:email_preference, news: false, likes: true, messages: true)
         put :update, format: :json, params: {email_preference: email_preference}
 
-        parsed_body = JSON.parse(response.body)
-        expect(parsed_body['email_preference']).to match(email_preference)
+        parsed_body = JSON.parse(response.body, symbolize_names: true)
+        expect(parsed_body[:email_preference]).to match(email_preference)
       end
     end
 
     context "when update failed" do
-      it "should return :unprocessable_entity with errors" do
+      it "returns :unprocessable_entity with errors" do
         allow_any_instance_of(EmailPreference).to receive(:update).and_return(false)
         put :update, format: :json, params: {email_preference: {"news": "test"}}
 
