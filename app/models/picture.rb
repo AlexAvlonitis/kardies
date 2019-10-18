7ace12d267b110290a66380681c8ccb4281b2a29
Alex <avlonitis@msn.com>
2019-10-18 18:27:06 +0100

    Add active_support to pictures galleries

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,7 @@ class Picture < ApplicationRecord
   belongs_to :gallery
   belongs_to :user
 
-  has_many_attached :pictures
+  has_one_attached :picture
 
   def picture_medium
     picture.variant resize: "300x300>" if picture.attached?
