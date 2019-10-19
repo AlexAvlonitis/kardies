846d0cf81997a184e42da3f6652365166396527c
Alex <avlonitis@msn.com>
2019-10-19 23:09:34 +0100

    migration pass

--- app/serializers/api/v1/picture_serializer.rb
+++ app/serializers/api/v1/picture_serializer.rb
@@ -6,13 +6,13 @@ module Api
       attributes :id, :picture, :picture_medium
 
       def picture
-        return unless object.picture.attached?
+        return unless object&.picture && object.picture.attached?
 
         rails_blob_url(object.picture)
       end
 
       def picture_medium
-        return unless object.picture.attached?
+        return unless object&.picture object.picture.attached?
 
         rails_representation_url(object.picture_medium)
       end
