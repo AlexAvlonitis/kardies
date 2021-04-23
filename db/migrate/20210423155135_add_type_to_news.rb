a469802fdd94910288ae6aec8da0856ae2474ec2
Alex <avlonitis@msn.com>
2021-04-23 20:47:51 +0100

    Remove default value meta migraiton

--- db/migrate/20210423155135_add_type_to_news.rb
+++ db/migrate/20210423155135_add_type_to_news.rb
@@ -2,7 +2,7 @@ class AddTypeToNews < ActiveRecord::Migration[6.0]
   def change
     remove_column :news, :title, :string
     remove_column :news, :body, :text
-    add_column :news, :meta, :json, default: {}
+    add_column :news, :meta, :json
     add_column :news, :type, :string
   end
 end
