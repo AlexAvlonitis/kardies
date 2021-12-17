df4de3205bdfc3bb7b6235f25489695ed0c368ee
alexavlonitis <avlonitis@msn.com>
2021-12-17 17:11:55 +0000

    Fix spec

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe UserDecorator do
   it { is_expected.to respond_to(:profile_picture) }
   it { is_expected.to respond_to(:profile_picture_medium) }
   it { is_expected.to respond_to(:profile_picture_thumb) }
-  it { is_expected.to respond_to(:after_create) }
   it { is_expected.to respond_to(:after_confirmation) }
-  it { is_expected.to respond_to(:before_destroy) }
+  it { is_expected.to respond_to(:destroy) }
+  it { is_expected.to respond_to(:save) }
   it { is_expected.to respond_to(:message_email_notification_allowed?) }
   it { is_expected.to respond_to(:profile_picture_attached?) }
 end
