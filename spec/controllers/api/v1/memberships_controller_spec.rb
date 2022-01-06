f8a50915fa704de31a7f7440acd511f63e6c6125
Alex <avlonitis@msn.com>
2022-01-06 15:21:42 +0000

    Add membership controller specs

--- spec/controllers/api/v1/memberships_controller_spec.rb
+++ spec/controllers/api/v1/memberships_controller_spec.rb
@@ -1,5 +1,101 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::MembershipsController, type: :controller do
+  login_user
 
+  let(:subscription) { { id: 1, expiry_date: '2020-1-1' } }
+  let(:params) do
+    {
+      payment_method: "card",
+      payment_plan: "monthly",
+      idempotency_key: 'random_key'
+    }
+  end
+
+  describe "POST #create" do
+    before do
+      allow(Memberships::CreateMembershipService).to receive(:call) { subscription }
+    end
+
+    context 'when the request is successful' do
+      it 'returns the subscription' do
+        post :create, params: params
+
+        assert_response :success
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body).to eq(subscription.with_indifferent_access)
+      end
+
+      it "calls the create membership service" do
+        a_params = ActionController::Parameters.new(params)
+        permitted = a_params.permit(*params.keys)
+
+        expect(Memberships::CreateMembershipService)
+          .to receive(:call)
+          .with(@user, permitted)
+
+        post :create, params: params
+      end
+    end
+
+    context 'when the request raises an errors' do
+      before do
+        allow(Memberships::CreateMembershipService)
+          .to receive(:call)
+          .and_raise(StandardError.new('any error message'))
+      end
+
+      it 'returns the error' do
+        post :create, params: params
+
+        assert_response :unprocessable_entity
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body).to eq({ 'errors' => 'any error message' })
+      end
+    end
+  end
+
+  [
+    [Memberships::UpdateMembershipService, 'store_membership', :post],
+    [Memberships::GetMembershipService, 'retrieve_membership', :get],
+    [Memberships::CancelMembershipService, 'destroy', :delete]
+  ].each do |service, method, action|
+    describe "#{action.to_s.upcase} ##{method}" do
+      before do
+        allow(service).to receive(:call) { subscription }
+      end
+
+      context 'when the request is successful' do
+        it 'returns the subscription' do
+          public_send(action, method.to_sym)
+
+          assert_response :success
+          parsed_body = JSON.parse(response.body)
+          expect(parsed_body).to eq(subscription.with_indifferent_access)
+        end
+
+        it "calls the #{service.to_s} service" do
+          expect(service).to receive(:call).with(@user)
+
+          public_send(action, method.to_sym)
+        end
+      end
+
+      context 'when the request raises an errors' do
+        before do
+          allow(service)
+            .to receive(:call)
+            .and_raise(StandardError.new('any error message'))
+        end
+
+        it 'returns the error' do
+          public_send(action, method.to_sym)
+
+          assert_response :unprocessable_entity
+          parsed_body = JSON.parse(response.body)
+          expect(parsed_body).to eq({ 'errors' => 'any error message' })
+        end
+      end
+    end
+  end
 end
